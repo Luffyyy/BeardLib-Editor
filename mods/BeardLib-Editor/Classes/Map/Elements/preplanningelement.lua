94c67002278b5649147444858bd79f8113da453b
Luffy <danielandro11@gmail.com>
2017-07-15 15:21:33 +0300

    Fixed PrePlanning element not saving types correctly.

--- mods/BeardLib-Editor/Classes/Map/Elements/preplanningelement.lua
+++ mods/BeardLib-Editor/Classes/Map/Elements/preplanningelement.lua
@@ -18,25 +18,18 @@ function EditorPrePlanning:_build_panel()
 	self:ComboCtrl("upgrade_lock", tweak_data.preplanning.upgrade_locks, {help =  "Select a upgrade lock from the combobox"})
 	self:ComboCtrl("dlc_lock", tweak_data.preplanning.dlc_locks, {help = "Select a DLC lock from the combobox"})
 	self:ComboCtrl("location_group", tweak_data.preplanning.location_groups, {help = "Select a location group from the combobox"})
-	local allowed_types = {}
-	for k, v in pairs(managers.preplanning:types()) do
-		allowed_types[v] = self._element.values.allowed_types[v] == true
-	end
-	local disables_types = {}
-	for k, v in pairs(managers.preplanning:types()) do
-		disables_types[v] = self._element.values.disables_types[v] == true
-	end
+	local types = managers.preplanning:types()
 	self:Button("SelectAllowedTypes", function()
 	    BeardLibEditor.managers.SelectDialog:Show({
 	        selected_list = self._element.values.allowed_types,
-	        list = allowed_types,
+	        list = types,
 	        callback = callback(self, self, "_data_updated", "allowed_types")
 	    })		
 	end)	
 	self:Button("SelectDisablesTypes", function()
 	    BeardLibEditor.managers.SelectDialog:Show({
 	        selected_list = self._element.values.disables_types,
-	        list = disables_types,
+	        list = types,
 	        callback = callback(self, self, "_data_updated", "disables_types")
 	    })		
 	end)
