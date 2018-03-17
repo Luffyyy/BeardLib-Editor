7c675e4a4b09c9090ec75d539459f4e45f82825c
ontrigger <firewarpcd@gmail.com>
2018-03-17 22:46:31 +0300

    fixed unit rotations, shortened the code

--- mods/BeardLib-Editor/Classes/Map/UndoUnitHandler.lua
+++ mods/BeardLib-Editor/Classes/Map/UndoUnitHandler.lua
@@ -6,14 +6,17 @@ local UHandler = UndoUnitHandler
 function UHandler:init()
     self._unit_data = {}
     self._undo_stack = CoreStack.Stack:new()
-    self._redo_stack = CoreStack.Stack:new()
+    self._redo_data = {}
     self._undo_history_size = BLE.Options:GetValue("UndoHistorySize")
 end
 
 function UHandler:SaveUnitValues(units, action_type)
     local jump_table = {
-        pos = function(u) table.insert(self._unit_data[u:key()].pos, 1, u:unit_data()._prev_pos) end,  -- I have to insert them into the first pos as
-        rot = function(u) table.insert(self._unit_data[u:key()].rot, 1, u:unit_data()._prev_rot) end   -- to make it easier to get the first inserted element
+        pos = function(u) table.insert(self._unit_data[u:key()].pos, 1, u:unit_data()._prev_pos) end,
+        rot = function(u)
+            table.insert(self._unit_data[u:key()].rot, 1, u:unit_data()._prev_rot)
+            table.insert(self._unit_data[u:key()].pos, 1, u:unit_data()._prev_pos)
+        end
     }
 
     if self._undo_stack:size() > self._undo_history_size then
@@ -44,32 +47,26 @@ function UHandler:SaveUnitValues(units, action_type)
 end
 
 function UHandler:Undo()
-    local jump_table = {
-        pos = function(k) self:restore_unit_pos(k) end,
-        rot = function(k) self:restore_unit_rot(k) end
-    }
-    if not self._undo_stack:is_empty() then
-        local element = self._undo_stack:pop()
-        for _, key in pairs(element[1]) do
-            jump_table[element[2]](key)
-        end
-        self._redo_stack:push(element)
-    else managers.editor:Log("Undo stack is empty!")
+    if self._undo_stack:is_empty() then
+        managers.editor:Log("Undo stack is empty!")
+        return
     end
 
+    local element = self._undo_stack:pop()
+    for _, key in pairs(element[1]) do
+        self:restore_unit_pos_rot(key, element[2])
+    end
 end
 
-function UHandler:restore_unit_pos(key)    -- with the current implementation i have to have 2 different methods for unit restore
+function UHandler:set_redo_values(key)
+--empty
+end
+
+function UHandler:restore_unit_pos_rot(key, action)
+    local action_string = action == "rot" and "rotation " or "to position "
     local pos = table.remove(self._unit_data[key].pos, 1)
-    managers.editor:Log("Restoring unit to position: " .. tostring(pos))
+    local rot = action == "rot" and table.remove(self._unit_data[key].rot, 1) or nil
     local unit = self._unit_data[key].unit_data
-    BLE.Utils:SetPosition(unit, pos)
-end
-
-function UHandler:restore_unit_rot(key)     -- unit rotation is completely fucked, and causes dumb crashes
-    local pos = self._unit_data[key].pos[1]
-    local rot = table.remove(self._unit_data[key].rot, 1)
-    managers.editor:Log("Restoring unit rotation: " .. tostring(rot))
-    local unit = self._unit_data[key].unit_data
+    managers.editor:Log("Restoring unit " .. action_string .. tostring(rot or pos))
     BLE.Utils:SetPosition(unit, pos, rot)
 end
