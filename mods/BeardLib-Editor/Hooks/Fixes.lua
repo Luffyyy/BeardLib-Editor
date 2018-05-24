2f9cec7d502fcfcdfee3eb165961721d3e83143d
Daniel.R <danielandro11@gmail.com>
2018-05-24 09:13:26 +0300

    Fixed #210

--- mods/BeardLib-Editor/Hooks/Fixes.lua
+++ mods/BeardLib-Editor/Hooks/Fixes.lua
@@ -10,7 +10,9 @@ if civ or F == "elementspawnenemydummy" then
 	local C = civ and ElementSpawnCivilian or ElementSpawnEnemyDummy
 	--Makes sure unit path is updated.
 	Hooks:PostHook(C, "_finalize_values", "EditorFinalizeValues", function(self)
-		self._enemy_name = self._values.enemy and Idstring(self._values.enemy) or nil
+		if self._values.enemy then
+			self._enemy_name = self._values.enemy and Idstring(self._values.enemy) or nil
+		end
 		if not self._enemy_name then
 			if civ then
 				self._enemy_name = Idstring("units/characters/civilians/dummy_civilian_1/dummy_civilian_1")
