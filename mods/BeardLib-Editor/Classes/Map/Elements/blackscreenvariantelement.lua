8abe2fce8cdd5462b500a060d8ff24838eb9521d
ontrigger <firewarpcd@gmail.com>
2018-08-12 01:19:07 +0300

    Fixed #249

--- mods/BeardLib-Editor/Classes/Map/Elements/blackscreenvariantelement.lua
+++ mods/BeardLib-Editor/Classes/Map/Elements/blackscreenvariantelement.lua
@@ -1,22 +1,22 @@
-EditorBlackScreenVariant = EditorBlackScreenVariant or class(MissionScriptEditor)
-function EditorBlackScreenVariant:create_element()
+EditorBlackscreenVariant = EditorBlackscreenVariant or class(MissionScriptEditor)
+function EditorBlackscreenVariant:create_element()
 	self.super.create_element(self)
 	self._element.class = "ElementBlackscreenVariant"
 	self._element.values.variant = "0" 
 end
-function EditorBlackScreenVariant:_build_panel()
+function EditorBlackscreenVariant:_build_panel()
 	self:_create_panel()
 	self:ComboCtrl("variant", self:_get_params(), "Select variant, from level_tweak_data.level.intro_event")
 end
-function EditorBlackScreenVariant:_get_params()
+function EditorBlackscreenVariant:_get_params()
 	local tbl = {}
 	for i=1, 30 do 
 		table.insert(tbl, tostring(i))
 	end
 	return tbl
 end
-EndscreenVariantElement = EndscreenVariantElement or class(EditorBlackScreenVariant)
-function EditorBlackScreenVariant:_build_panel()
+EndscreenVariantElement = EndscreenVariantElement or class(EditorBlackscreenVariant)
+function EditorBlackscreenVariant:_build_panel()
 	self:_create_panel()
 	self:ComboCtrl("variant", self:_get_params(), {
 		help = "Select variant, from level_tweak_data.level.outro_event", 
