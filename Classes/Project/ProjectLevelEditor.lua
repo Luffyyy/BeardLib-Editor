a871bd2bd1433910fac25c376eb9cd916f96d271
Daniel <danielandro11@gmail.com>
2020-01-24 04:59:49 +0200

    Initial code for cloning levels

--- Classes/Project/ProjectLevelEditor.lua
+++ Classes/Project/ProjectLevelEditor.lua
@@ -60,25 +60,166 @@ function ProjectLevelEditor:create(create_data)
             return warn == nil
         end,
         callback = function(name)
-            local template = FileIO:ReadScriptData(Path:Combine(BLE.MapProject._templates_directory, "LevelModule.xml"), "custom_xml", true)
-            template.id = name
-            local proj_path = self._parent:get_dir()
-            local level_path = Path:Combine("levels", template.id)
-            template.include.directory = level_path
-
+            local template
             if create_data.clone then
-                --TODO: clone code
+                create_data.name = name
+                template = self:clone_level(create_data)
             else
-                if create_data.narrative then
-                    table.insert(create_data.narrative, {level_id = level.id, type = "d", type_id = "heist_type_assault"})
-                end
+                template = deep_clone(BLE.MapProject._level_module_template)
+                template.id = name
+                local proj_path = self._parent:get_dir()
+                local level_path = Path:Combine("levels", template.id)
+                template.include.directory = level_path
 
                 FileIO:MakeDir(Path:Combine(proj_path, level_path))
                 FileIO:CopyToAsync(Path:Combine(BLE.MapProject._templates_directory, "Level"), Path:Combine(proj_path, level_path))
             end
+            --If we need to insert this into a chain of a narrative
+            if create_data.narrative then
+                table.insert(create_data.narrative, {level_id = level.id, type = "d", type_id = "heist_type_assault"})
+            end
             self:finalize_creation(template)
         end
     })
+end
+
+function ProjectLevelEditor:clone_level(create_data)
+    local clone_id = create_data.clone_id
+    local level = clone(tweak_data.levels[clone_id])
+    local name = create_data.name
+    local packages = type(level.package) == "string" and {level.package} or level.package or {}
+    local level_dir = "levels/"..level.world_name .. "/"
+
+    --Merge module stuff into the cloned level tweakdata entry
+    table.merge(level, {
+        _meta = "level",
+        assets = {},
+        id = name,
+        add = {directory = "assets"},
+        include = {directory = Path:Combine("levels", name)},
+        packages = packages,
+        script_data_mods = deep_clone(BLE.MapProject._level_module_template).script_data_mods
+    })
+
+    --Clone preplanning
+    local preplanning = tweak_data.preplanning.locations[id]
+    if preplanning then
+        level.preplanning = deep_clone(preplanning)
+    end
+
+    local function extra_package(p)
+        if not table.contains(packages, p) then
+            table.insert(packages, p)
+        end
+    end
+
+    --Search for the narrative package if exists, some levels may depend on it.
+    local found_narr = false
+    for _, narr in pairs(tweak_data.narrative.jobs) do
+        if found_narr then
+            break
+        end
+        if narr.chain then
+            for _, chain_level in ipairs(narr.chain) do
+                if found_narr then
+                    break
+                end
+                if level.level_id then
+                    if chain_level.level_id == clone_id then
+                        extra_package(narr.package)
+                        found_narr = true
+                        break
+                    end
+                else
+                    for _, inner_chain_level in pairs(chain_level) do
+                        if inner_chain_level.level_id == clone_id then
+                            extra_package(narr.package)
+                            found_narr = true
+                            break
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+
+    local custom_level_dir = Path:Combine(self._parent:get_dir(), "levels", name)
+
+    --This local function is used to extract the files of the map by reading the scriptdata.
+    --To actually have it work we ofc need to load the packages first or else the game will go into shit.
+    local function extra_file(name, typ, path, data_func)
+        path = path or level_dir
+        typ = typ or name
+        local data
+        local typeid = typ:id()
+        local nameid = name:id()
+        local inpath = Path:Combine(path, name)
+        if PackageManager:has(typeid, inpath:id()) then
+            data = PackageManager:script_data(typeid, inpath:id())
+            if data_func then
+                data_func(data)
+            end
+            local infolder = path:gsub(level_dir, "")                
+            local file = (infolder:len() > 0 and infolder.."/" or infolder) .. name.."."..typ
+            FileIO:WriteScriptData(Path:Combine(custom_level_dir, file), data, "binary")
+            table.insert(level.include, {_meta = "file", file = file, type = "binary"})
+        else
+            BLE:log("[add_existing_level_to_project][extra_file] File is unloaded %s", inpath)
+        end
+        if PackageManager:package_exists(inpath) then
+            extra_package(inpath)
+        end
+        return data
+    end
+
+    --Here we go through possible files of the map to extract them.
+    extra_package(level_dir.."world")
+    for _, p in pairs(packages) do
+        self:load_temp_package(p.."_init")
+        self:load_temp_package(p)
+    end
+
+    extra_file("world", nil, nil, function(data) data.brush = nil end)
+    local continents_data = extra_file("continents")
+    extra_file("mission")
+    extra_file("nav_manager_data", "nav_data")
+    extra_file("cover_data")
+    extra_file("world_sounds")
+    extra_file("world_cameras")
+    extra_file("world_cameras")
+    extra_file("blacklist")
+
+    --Here we extract the cube lights. There is no need to load any packages as binary files such as textures are easily extractable.
+    local cube_lights_dir = Path:Combine(level_dir, "cube_lights")
+    local texture = "texture"
+    local file_path
+    local add = {_meta = "add", directory = "assets"}
+    for k, v  in pairs(Global.DBPaths[texture]) do
+        if v and k:sub(1, #cube_lights_dir) == inpath then 
+            file_path = Path:Combine("levels/mods", name, string.sub(k, #path))
+            FileIO:WriteTo(Path:Combine(BeardLib.config.maps_dir, data.name, "assets", file_path) .. "." .. typ , DB:open(texture, k):read())
+            table.insert(add, {_meta = "texture", path = file_path})
+        end
+    end
+
+    --Write to the add.xml of the level
+    FileIO:WriteScriptData(Path:Combine(custom_level_dir, "add.xml"), add, CXML)
+
+    for c in pairs(continents_data) do
+        local c_path = Path:Combine(level_dir, c)
+        self:load_temp_package(Path:Combine(c_path, c).."_init")
+        extra_file(c, "continent", c_path)
+        extra_file(c, "mission", c_path)
+    end
+
+    --Removing stuff that the module doesn't require/use.
+    level.world_name = nil
+    level.name_id = nil
+    level.briefing_id = nil
+    level.package = nil
+
+    return level
 end
 
 --- Opens a dialog for editing the mission assets of a level
