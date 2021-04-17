c3b7bd694d4fbc72c0402b8ac51074ab5d928bb9
Daniel R <luffydafloffi@gmail.com>
2021-04-17 21:47:56 +0300

    Last loaded level button

---
+++ Hooks/LoadLastLevelButton.lua
@@ -0,0 +1,64 @@
+Hooks:PostHook(BLTNotificationsGui, "_setup", "BLESetup", function(self)
+    self._beardlib_editor_menu = self._beardlib_panel:bitmap({
+        name = "BeardlibEditorMenu",
+        texture = "textures/editor_icons_df",
+        texture_rect = {256, 262, 115, 115},
+        w = 28,
+        h = 28,
+        y = 8,
+        x = self._beardlib_achievements:right() + 4,
+        color = self._beardlib_accent
+    })
+    local data = BLE.Options:GetValue("LastLoaded")
+    if data then
+        self._beardlib_editor_startlast = self._beardlib_panel:bitmap({
+            name = "RestartLastEditorHeist",
+            texture = "textures/editor_icons_df",
+            texture_rect = {116, 110, 32, 32},
+            w = 28,
+            h = 28,
+            y = 8,
+            x = self._beardlib_editor_menu:right() + 4,
+            color = self._beardlib_accent
+        })
+    end
+end)
+
+local mouse_move = BLTNotificationsGui.mouse_moved
+function BLTNotificationsGui:mouse_moved(o, x, y)
+    if not self._enabled then
+        return
+    end
+
+    if alive(self._beardlib_editor_startlast) and alive(self._beardlib_editor_menu) then
+        if self._beardlib_editor_startlast:inside(x,y) or self._beardlib_editor_menu:inside(x,y) then
+            return true, "link"
+        end
+    end
+    return mouse_move(self, x, y)
+end
+
+local mouse_press = BLTNotificationsGui.mouse_pressed
+function BLTNotificationsGui:mouse_pressed(button, x, y)
+    if not self._enabled or button ~= Idstring("0") then
+        return
+    end
+    if BLE then
+        if alive(self._beardlib_editor_startlast) and self._beardlib_editor_startlast:inside(x,y) then
+            local data = BLE.Options:GetValue("LastLoaded")
+            if data and data.name then
+                if tweak_data.levels[data.name] then
+                    BLE.LoadLevel:load_level(data)
+                else
+                    BeardLib.Managers.Dialog:Simple():Show({title = managers.localization:text("mod_assets_error"), message = "Heist \"" .. data.name .. "\" not found.", force = true})
+                end
+            end
+            return true
+        end
+        if alive(self._beardlib_editor_menu) and self._beardlib_editor_menu:inside(x,y) then
+            BLE.Menu:set_enabled(true)
+            return true
+        end
+    end
+    return mouse_press(self, button, x, y)
+end
