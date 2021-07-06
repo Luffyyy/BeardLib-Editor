0d48e7e390e9a6c624709b27469d84b56845a1cf
Daniel R <luffydafloffi@gmail.com>
2021-07-06 04:57:56 +0300

    Adjusted hints more

--- Classes/MenuUIExt.lua
+++ Classes/MenuUIExt.lua
@@ -236,13 +236,13 @@ function ItemExt:colorbox(name, callback, value, o)
 end
 
 function ItemExt:alert(text, color)
-	local div = self:lbl(text, {color = true, private = {background_color = self.full_bg_color}, text_offset = {4, 4, 28, 4}, border_color = color or Color.yellow, border_lock_height = false})
+	local div = self:lbl(text, {color = true, private = {background_color = self.full_bg_color}, highlight_color = self.full_bg_color, text_offset = {4, 4, 32, 4}, border_color = color or Color.yellow, border_lock_height = false})
 	div:tb_imgbtn("Alert", nil, nil, BLE.Utils.EditorIcons.alert, {divider_type = true, img_scale = 0.8, w = 24, h = 24})
 	return div
 end
 
 function ItemExt:info(text, color)
-	local div = self:lbl(text, {color = true, private = {background_color = self.full_bg_color}, text_offset = {4, 4, 28, 4}, border_lock_height = false})
+	local div = self:lbl(text, {color = true, private = {background_color = self.full_bg_color}, highlight_color = self.full_bg_color, text_offset = {4, 4, 32, 4}, border_lock_height = false})
 	div:tb_imgbtn("Info", nil, nil, BLE.Utils.EditorIcons.help, {divider_type = true, img_scale = 0.8, w = 24, h = 24})
 	return div
 end
