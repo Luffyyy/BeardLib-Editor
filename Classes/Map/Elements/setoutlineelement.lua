a0fb7a0283849782040910f1cfb2d1af7695a925
Xeletron <xeletron1@gmail.com>
2021-12-05 18:45:52 +0100

    Added missing values for the SetOutline element

--- Classes/Map/Elements/setoutlineelement.lua
+++ Classes/Map/Elements/setoutlineelement.lua
@@ -4,6 +4,8 @@ function EditorSetOutline:create_element()
 	self._element.class = "ElementSetOutline"
 	self._element.values.elements = {}
 	self._element.values.set_outline = true
+	self._element.values.use_instigator = false
+	self._element.values.clear_previous = false
 end
 function EditorSetOutline:_build_panel()
 	self:_create_panel()
@@ -12,5 +14,7 @@ function EditorSetOutline:_build_panel()
 		"ai_spawn_civilian"
 	}
 	self:BuildElementsManage("elements", nil, {"ElementSpawnEnemyDummy", "ElementSpawnCivilian"})
-	 self:BooleanCtrl("set_outline", {text = "Enable outline"})
+	self:BooleanCtrl("set_outline", {text = "Enable outline"})
+	self:BooleanCtrl("use_instigator", {text = "Instigator only"})
+	self:BooleanCtrl("clear_previous", {text = "Clear previous outlines"})
 end
