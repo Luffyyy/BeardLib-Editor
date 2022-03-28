9b2960db1d520407a5e6f8d68f7c99ca7757cebc
Patonay Patrik <xeletron1@gmail.com>
2022-03-29 01:53:52 +0200

    Fixed Endscreen Variant element not working

--- Classes/Map/Elements/blackscreenvariantelement.lua
+++ Classes/Map/Elements/blackscreenvariantelement.lua
@@ -6,7 +6,7 @@ function EditorBlackscreenVariant:create_element()
 end
 function EditorBlackscreenVariant:_build_panel()
 	self:_create_panel()
-	self:ComboCtrl("variant", self:_get_params(), "Select variant, from level_tweak_data.level.intro_event")
+	self:ComboCtrl("variant", self:_get_params(), {help = "Select variant, from level_tweak_data.level.intro_event"})
 end
 function EditorBlackscreenVariant:_get_params()
 	local tbl = {}
@@ -15,12 +15,15 @@ function EditorBlackscreenVariant:_get_params()
 	end
 	return tbl
 end
-EndscreenVariantElement = EndscreenVariantElement or class(EditorBlackscreenVariant)
-function EditorBlackscreenVariant:_build_panel()
-	self:_create_panel()
-	self:ComboCtrl("variant", self:_get_params(), {
-		help = "Select variant, from level_tweak_data.level.outro_event", 
-		text = "Endscreen variant:"
-	})
+
+EditorEndscreenVariant = EditorEndscreenVariant or class(EditorBlackscreenVariant)
+function EditorEndscreenVariant:create_element()
+	EditorBlackscreenVariant.super.create_element(self)
+	self._element.class = "ElementEndscreenVariant"
+	self._element.values.variant = "0" 
 end
- 
+
+function EditorEndscreenVariant:_build_panel()
+	self:_create_panel()
+	self:ComboCtrl("variant", self:_get_params(), {help = "Select variant, from level_tweak_data.level.outro_event"})
+end
